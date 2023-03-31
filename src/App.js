c93013cd5e6a539a02df674599548d1333dc6ae7
ber <yanqinglou@outlook.com>
2023-03-30 22:25:07 -0700

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, {useState, useEffect, useRef} from "react";
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from "./Components/Header/index.js"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+      </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
